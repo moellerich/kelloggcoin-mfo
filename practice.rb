93d4a5ebaf28d6076b72eb7d176dc5502864f8e8
Matthew Oellerich <matthew.oellerich@kellogg.northwestern.edu>
2024-02-03 12:33:40 +0000

    KelloggCoin practice

--- practice.rb
+++ practice.rb
@@ -27,3 +27,55 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+# create 4 variables to represent each user's balance
+
+ben_balance = 0
+brian_balance = 0
+evan_balance = 0
+anthony_balance = 0
+
+# loop through the blockchain transactions and update the balances accordingly
+
+index = 0
+
+loop do
+  if index == blockchain.length
+    break
+  end
+  transaction = blockchain[index]
+
+  # if the user is the "from_user" decrement their balance
+  
+    if transaction["from_user"] == "ben"
+      ben_balance = ben_balance - transaction["amount"]
+    elsif transaction["from_user"] == "brian"
+      brian_balance = brian_balance - transaction["amount"]
+    elsif transaction["from_user"] == "anthony"
+      anthony_balance = anthony_balance - transaction["amount"]
+    elsif transaction["from_user"] == "evan"
+      evan_balance = evan_balance - transaction["amount"]
+    end
+
+  # if the user is the "to_user" increment their balance
+
+  if transaction["to_user"] == "ben"
+    ben_balance = ben_balance + transaction["amount"]
+  elsif transaction["to_user"] == "brian"
+    brian_balance = brian_balance + transaction["amount"]
+  elsif transaction["to_user"] == "anthony"
+    anthony_balance = anthony_balance + transaction["amount"]
+  elsif transaction["to_user"] == "evan"
+    evan_balance = evan_balance + transaction["amount"]
+  end
+
+index = index + 1
+
+end
+
+# print out the results
+
+puts "Ben's KelloggCoin balance is #{ben_balance}"
+puts "Brian's KelloggCoin balance is #{brian_balance}"
+puts "Evan's KelloggCoin balance is #{evan_balance}"
+puts "Anthony's KelloggCoin balance is #{anthony_balance}"
